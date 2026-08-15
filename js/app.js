becd619bf8751c43ad6dac02195aff07b4fb7c66
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-08-15 11:06:51 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+import { auth } from './firebase-init.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { verificarLoginEExibirTela } from './auth.js';
 
+// Monitora se o usuário está logado
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        verificarLoginEExibirTela(user);
+    } else {
+        // Redirecionar para tela de login
+        console.log("Usuário não logado");
+        // Aqui você chamará a função que desenha o formulário de login
+    }
+});
